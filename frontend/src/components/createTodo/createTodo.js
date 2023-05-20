43541dbbaaa9b6d69ea96ef3f035169c34cf2a52
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-05-20 22:42:53 +0500

    background image added

---
+++ frontend/src/components/createTodo/createTodo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+    <div>createTODO</div>
+  )
+}
+
+export default CreateTodo
---
+++ frontend/src/components/viewAllTodos/viewAllTodos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ViewAllTodos = () => {
+  return (
+    <div>viewAllTodos</div>
+  )
+}
+
+export default ViewAllTodos
