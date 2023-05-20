731aa902bff30097903636eb287a24690aeb3c7c
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-05-20 15:47:56 +0500

    db connection added + basic file structure added

---
+++ backend/connection/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+try{
+    mongoose.connect(
+        process.env.DATABASE_URL,
+        { useNewUrlParser: true },
+        
+      );
+      console.log("db connected successfully")
+}catch(err){
+    console.log(err)
+}
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const express = require("express")
 const cors = require('cors')
+require("./connection/connection");
 const app = express()
 
 app.use(express.json())
