3308730d2da30271ec5b9c293fc59ae432b17dcd
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-05-23 00:20:01 +0500

    most of coveraged added for tests

--- backend/__test__/todoController.test.js
+++ backend/__test__/todoController.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest");
 const app = require("../index");
-const { createTodo, getAllTodos } = require("../dao/todoDao");
+const { createTodo, getAllTodos,removeTodo,searchTodo } = require("../dao/todoDao");
 jest.mock("../dao/todoDao");
 describe("testing endpoints for todo controller", () => {
   describe("testing create todo endpoint", () => {
@@ -26,12 +26,41 @@ describe("testing endpoints for todo controller", () => {
           creationTime: new Date(),
         };
       });
-      const response = await request(app).get("/view").send({
-        task: "testing",
-        completed: false,
-        creationTime: new Date(),
-      });
+      const response = await request(app).get("/view")
       expect(response.statusCode).toBe(200);
     });
+  });
+  describe("testing delete todo endpoint", () => {
+    test("should return status code of 400, if taskId is not provided", async () => {
+      getAllTodos.mockImplementationOnce(() => {
+        return {
+          task: "testing",
+          completed: false,
+          creationTime: new Date(),
+        };
+      });
+      const response = await request(app).delete(`/delete/${null}`)
+      expect(response.statusCode).toBe(400);
+    });
+    test("should return status code of 400, if task is not found against given taskId", async () => {
+        searchTodo.mockImplementationOnce(() => {
+          return null
+        });
+        const response = await request(app).delete(`/delete/646b888366119b3106864527`)
+        expect(response.statusCode).toBe(400);
+      });
+      
+      test("should return status code of 200, if task is found against given taskId", async () => {
+        searchTodo.mockImplementationOnce(() => {
+          return {
+            task: "testing",
+            completed: false,
+            creationTime: new Date(),
+          }
+        });
+        const response = await request(app).delete(`/delete/646b888366119b3106864527`)
+        expect(response.statusCode).toBe(200);
+      });
+      
   });
 });
