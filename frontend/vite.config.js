b99960234c54ac2dd143da0bf9fe5a010be922af
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-06-04 18:27:05 +0500

    dir strcture updated + imports corrected

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     react(), 
@@ -10,8 +10,8 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    host: true, // needed for the Docker Container port mapping to work
+    host: true,
     strictPort: true,
-    port: 5173, // you can replace this port with any port
+    port: 5173,
   }
 })
